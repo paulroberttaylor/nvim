03a6949eb7bdd63c488ff0d2cfc3acf0bd2a6a16
Paul Taylor <paul.taylor.external@ynap.com>
2024-01-17 13:54:38 +0000

    Display line numbers and relative line numbers.

--- init.lua
+++ init.lua
@@ -4,6 +4,10 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
 
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "number"
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
