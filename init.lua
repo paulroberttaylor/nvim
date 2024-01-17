f73c5b948f99f43c137ce8109cb6de850093c058
Paul Taylor <paul.taylor.external@ynap.com>
2024-01-17 13:46:40 +0000

    Amend git blame text

--- init.lua
+++ init.lua
@@ -71,7 +71,7 @@ require("gitsigns").setup({
         changedelete = { text = '~' },
         untracked    = { text = '┆' },
     },
-    current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d %HH-%MM> - <summary>',
+    current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d %H:%M> - <summary>',
     current_line_blame = true,
 })
 
