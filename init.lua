283268328c0a09cfb75689c84a586ce4a5302e2b
Paul Taylor <paul.taylor.external@ynap.com>
2024-01-17 13:15:32 +0000

    Added neotree with keymap.

--- init.lua
+++ init.lua
@@ -26,7 +26,16 @@ local plugins = {
     {
         "nvim-treesitter/nvim-treesitter", 
         build = ":TSUpdate" 
-    }
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        }
+    },
 }
 local opts = {}
 
@@ -35,6 +44,7 @@ require("lazy").setup(plugins, opts)
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal right<CR>')
 
 local config = require("nvim-treesitter.configs")
 config.setup({
